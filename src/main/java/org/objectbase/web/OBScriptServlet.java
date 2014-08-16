ef5a012ecc71534996e3e44437a00e0c18e7761d
xieleigang <xieleigang@gmail.com>
2014-08-16 16:58:37 +0800

    jsp and servlet

--- src/main/java/org/objectbase/web/OBScriptServlet.java
+++ src/main/java/org/objectbase/web/OBScriptServlet.java
@@ -1,5 +1,32 @@
 package org.objectbase.web;
 
-public class OBScriptServlet {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class OBScriptServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6069528808503118059L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) {
+		doPost(req, resp);
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+		String script = req.getParameter("script");
+		script = script == null ? "" : script.trim();
+		System.out.println("receive script: \n"+script);
+		Object result = "success";
+		try {
+			req.setAttribute("orign", script);
+			req.setAttribute("result", result == null ? "" : result.toString());
+			req.getRequestDispatcher("/index.jsp").forward(req, resp);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Objectbase Console</title>
+</head>
+<body>
+	<form method="post" action="console" style="text-align:center; vertical-align:middle;">
+		<textarea name="script" rows="30" cols="100">${origin}</textarea>
+		<br/>
+		<input type="submit" value="Execute"/>
+		<br/>
+		<textarea name="result" rows="10" cols="100" readonly="readonly">${result}</textarea>
+	</form>
+</body>
+</html>
